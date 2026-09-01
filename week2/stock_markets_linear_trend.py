7f5995072c6e41cac05826742cdb2424722687b0
verystark <tuomaspaloheimo05@gmail.com>
2026-09-01 14:15:04 +0300

    reconstruct whole program

--- week2/stock_markets_linear_trend.py
+++ week2/stock_markets_linear_trend.py
@@ -2,14 +2,16 @@ import yfinance as yf
 
 import matplotlib.pyplot as plt
 
-tickers = ['NDA-FI.HE', 'NOKIA.HE', 'KNEBV.HE', 'SAMPO.HE', 'NESTE.HE',
+def main():
+    tickers = ['NDA-FI.HE', 'NOKIA.HE', 'KNEBV.HE', 'SAMPO.HE', 'NESTE.HE',
            'FORTUM.HE', 'WRT1V.HE', 'METSO.HE', 'UPM.HE', 'ORNBV.HE',
            'KESKOB.HE', 'STERV.HE', 'KCR.HE', 'ELISA.HE', 'VALMT.HE',
            'HIAB.HE', 'HUH1V.HE', 'MANTA.HE', 'OUT1V.HE', 'KEMIRA.HE',
            'TYRES.HE', 'LUMO.HE', 'TIETO.HE', 'BITTI.HE', 'QTCOM.HE'
            ]
-
+    
+    for i in tickers:
+        data = yf.download(i, '2022-02-24', '2026-08-31', auto_adjust=False, threads=False, progress=False)
 
 if __name__ == '__main__':
-    for i in tickers:
-        data = yf.download(i, '2022-02-24', '2026-08-31', auto_adjust=False, threads=False)
+    main()
