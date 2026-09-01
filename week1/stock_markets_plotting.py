4d2d0b53c95d6ac2658a1a7bcf09b0663562bbce
verystark <tuomaspaloheimo05@gmail.com>
2026-09-01 18:58:33 +0300

    make x and y values from data

--- week1/stock_markets_plotting.py
+++ week1/stock_markets_plotting.py
@@ -2,11 +2,11 @@ import yfinance as yf
 
 import matplotlib.pyplot as plt
 
-tickers = ['NDA-FI.HE', 'NOKIA.HE', 'KNEBV.HE', 'SAMPO.HE', 'NESTE.HE',
-           'FORTUM.HE', 'WRT1V.HE', 'METSO.HE', 'UPM.HE', 'ORNBV.HE',
-           'KESKOB.HE', 'STERV.HE', 'KCR.HE', 'ELISA.HE', 'VALMT.HE',
-           'HIAB.HE', 'HUH1V.HE', 'MANTA.HE', 'OUT1V.HE', 'KEMIRA.HE',
-           'TYRES.HE', 'LUMO.HE', 'TIETO.HE', 'BITTI.HE', 'QTCOM.HE'
+tickers = ['NDA-FI.HE'#, 'NOKIA.HE', 'KNEBV.HE', 'SAMPO.HE', 'NESTE.HE',
+           #'FORTUM.HE', 'WRT1V.HE', 'METSO.HE', 'UPM.HE', 'ORNBV.HE',
+           #'KESKOB.HE', 'STERV.HE', 'KCR.HE', 'ELISA.HE', 'VALMT.HE',
+           #'HIAB.HE', 'HUH1V.HE', 'MANTA.HE', 'OUT1V.HE', 'KEMIRA.HE',
+           #'TYRES.HE', 'LUMO.HE', 'TIETO.HE', 'BITTI.HE', 'QTCOM.HE'
            ]
 
 best_company = ''
--- week2/stock_markets_linear_trend.py
+++ week2/stock_markets_linear_trend.py
@@ -2,16 +2,28 @@ import yfinance as yf
 
 import matplotlib.pyplot as plt
 
+def my_linfit(x, y):
+    a = 0
+    b = 0
+    return a, b
+
 def main():
-    tickers = ['NDA-FI.HE', 'NOKIA.HE', 'KNEBV.HE', 'SAMPO.HE', 'NESTE.HE',
-           'FORTUM.HE', 'WRT1V.HE', 'METSO.HE', 'UPM.HE', 'ORNBV.HE',
-           'KESKOB.HE', 'STERV.HE', 'KCR.HE', 'ELISA.HE', 'VALMT.HE',
-           'HIAB.HE', 'HUH1V.HE', 'MANTA.HE', 'OUT1V.HE', 'KEMIRA.HE',
-           'TYRES.HE', 'LUMO.HE', 'TIETO.HE', 'BITTI.HE', 'QTCOM.HE'
+    tickers = ['NDA-FI.HE'#, 'NOKIA.HE', 'KNEBV.HE', 'SAMPO.HE', 'NESTE.HE',
+           #'FORTUM.HE', 'WRT1V.HE', 'METSO.HE', 'UPM.HE', 'ORNBV.HE',
+           #'KESKOB.HE', 'STERV.HE', 'KCR.HE', 'ELISA.HE', 'VALMT.HE',
+           #'HIAB.HE', 'HUH1V.HE', 'MANTA.HE', 'OUT1V.HE', 'KEMIRA.HE',
+           #'TYRES.HE', 'LUMO.HE', 'TIETO.HE', 'BITTI.HE', 'QTCOM.HE'
            ]
-    
-    for i in tickers:
-        data = yf.download(i, '2022-02-24', '2026-08-31', auto_adjust=False, threads=False, progress=False)
+
+    for company in tickers:
+        data = yf.download(company, '2022-02-24', '2026-08-31', auto_adjust=False, threads=False, progress=False)
+        adj_close = data['Adj Close']
+        y = adj_close[company]
+        x = []
+        for i in range(y.size):
+            x.append(i)
+
+        a, b = my_linfit(x, y)
 
 if __name__ == '__main__':
     main()
