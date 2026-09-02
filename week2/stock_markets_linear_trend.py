0e5a72fb226a2bcbb6508bb0fd1dbf2720065872
verystark <tuomaspaloheimo05@gmail.com>
2026-09-02 19:33:56 +0300

    add function to determinen three most underrated companies based on stock performance vs. trend line

--- week2/stock_markets_linear_trend.py
+++ week2/stock_markets_linear_trend.py
@@ -2,6 +2,9 @@ import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
 
+def underrated_companies(companies_vs_trend):
+    return companies_vs_trend.argsort()[:3]
+
 def plus_or_minus(performance):
     return '+' if performance > 0 else ''
 
@@ -19,6 +22,7 @@ def main():
            'TYRES.HE', 'LUMO.HE', 'TIETO.HE', 'BITTI.HE', 'QTCOM.HE'
            ]
 
+    companies_vs_trend = []
     for company in tickers:
         data = yf.download(company, '2022-02-24', '2026-08-31', auto_adjust=False, threads=False, progress=False)
         adj_close = data['Adj Close']
@@ -33,11 +37,16 @@ def main():
         performance = (y.iloc[-1] / y.iloc[0] - 1) * 100
         last_linreg = (a*x+b)[-1]
         performance_vs_trend = (y.iloc[-1] / last_linreg - 1) * 100
+        companies_vs_trend.append(performance_vs_trend)
 
         adj_close.plot()
         plt.plot(data.index, a*x+b)
         plt.title(f'Stock performace: {plus_or_minus(performance)}{performance:.2f}%, Stock vs. linear trend: {plus_or_minus(performance_vs_trend)}{performance_vs_trend:.2f}%')
         plt.show()
+
+    # Determine and print the three most underrated companies
+    top_three = underrated_companies(np.array(companies_vs_trend))
+    print(f'three most underrated companies: {tickers[top_three[0]]}, {tickers[top_three[1]]} and {tickers[top_three[2]]}')
 
 if __name__ == '__main__':
     main()
