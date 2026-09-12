bc5d3f2dd0d41629ad23b4be716e765a11adc24d
verystark <tuomaspaloheimo05@gmail.com>
2026-09-12 21:54:26 +0300

    delete sanity check

--- week3/image_classification.py
+++ week3/image_classification.py
@@ -16,16 +16,6 @@ def my_1nn(x_train, y_train, x_test):
 
     return y_train[labels_index]
 
-# function for doing sanity check to accuracy calculation    
-def sanity_check():
-    pred = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    print('Do 10 sanity checks:')
-    for i in range(10):
-        gt = []
-        for i in range(10):
-            gt.append(random.randrange(0, 10))
-        print(f'sanity check: {my_cl_acc(pred, gt)}')
-
 # calculating accuracy of classifier
 def my_cl_acc(pred, gt):
     correct_class = 0
@@ -45,7 +35,7 @@ def main():
         x_test = pickle.load(data_file)
         y_test = pickle.load(data_file)
 
-    pred = my_1nn(x_train, y_train, x_test[:100])
+    pred = my_1nn(x_train, y_train, x_test[:500])
 
     end_time = datetime.now()
     processing_time = end_time - start_time
@@ -54,7 +44,7 @@ def main():
 
     print('Classifying images...')
     print(f'Processing time: {minutes}min {seconds}s for')
-    print(f'1_NN classification accuracy is {my_cl_acc(pred, y_test[:100])}')
+    print(f'1_NN classification accuracy is {my_cl_acc(pred, y_test[:500])}')
 
 if __name__ == "__main__":
     main()
