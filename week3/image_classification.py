b9849c4e62369a92e2b9d887edb3c0be63e8f13c
verystark <tuomaspaloheimo05@gmail.com>
2026-09-12 20:54:46 +0300

    made 1NN classification model faster by eliminating for loop with vectorization

--- week3/image_classification.py
+++ week3/image_classification.py
@@ -1,9 +1,9 @@
 import pickle
-import matplotlib.pyplot as plt
 import random
 import numpy as np
 from datetime import datetime
 
+# classify test data based on training data using 1NN
 def my_1nn(x_train, y_train, x_test):
 
     # flatten training data to 60000x784 matrix
@@ -12,19 +12,11 @@ def my_1nn(x_train, y_train, x_test):
 
     labels_index = []
     for i in x_test:
-        distance_all_pixels = []
-        for j in x_train:
-            distance = sum(np.sqrt((j-i)**2))
-            distance_all_pixels.append(distance)
-        labels_index.append(np.argmin(distance_all_pixels))
+        labels_index.append(np.argmin(np.sum(np.sqrt((x_train-i)**2), axis=1)))
 
-    labels_pred = []
-    for i in labels_index:
-        labels_pred.append(y_train[i])
+    return y_train[labels_index]
 
-    return labels_pred
-
-    
+# function for doing sanity check to accuracy calculation    
 def sanity_check():
     pred = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     print('Do 10 sanity checks:')
@@ -34,6 +26,7 @@ def sanity_check():
             gt.append(random.randrange(0, 10))
         print(f'sanity check: {my_cl_acc(pred, gt)}')
 
+# calculating accuracy of classifier
 def my_cl_acc(pred, gt):
     correct_class = 0
     for i in range(len(pred)):
@@ -52,18 +45,16 @@ def main():
         x_test = pickle.load(data_file)
         y_test = pickle.load(data_file)
 
-    sanity_check()
-
     pred = my_1nn(x_train, y_train, x_test[:100])
 
     end_time = datetime.now()
     processing_time = end_time - start_time
     minutes = processing_time.seconds // 60
     seconds = processing_time.seconds % 60
-    
+
+    print('Classifying images...')
     print(f'Processing time: {minutes}min {seconds}s for')
     print(f'1_NN classification accuracy is {my_cl_acc(pred, y_test[:100])}')
-
 
 if __name__ == "__main__":
     main()
