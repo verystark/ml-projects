878fca8bafa380a8643957b3f5034ff610358d1a
verystark <tuomaspaloheimo05@gmail.com>
2026-09-17 09:21:28 +0300

    add project stock_market_prediction.py

---
+++ week4/stock_market_prediction.py
@@ -0,0 +1,11 @@
+
+def main():
+    tickers = ['NDA-FI.HE', 'NOKIA.HE', 'KNEBV.HE', 'SAMPO.HE', 'NESTE.HE',
+           'FORTUM.HE', 'WRT1V.HE', 'METSO.HE', 'UPM.HE', 'ORNBV.HE',
+           'KESKOB.HE', 'STERV.HE', 'KCR.HE', 'ELISA.HE', 'VALMT.HE',
+           'HIAB.HE', 'HUH1V.HE', 'MANTA.HE', 'OUT1V.HE', 'KEMIRA.HE',
+           'TYRES.HE', 'LUMO.HE', 'TIETO.HE', 'BITTI.HE', 'QTCOM.HE'
+           ]
+
+if __name__ == "__main__":
+    main()
