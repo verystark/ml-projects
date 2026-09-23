9dacf181d3a1afed6ebd63366df29bf9b36a335a
verystark <tuomaspaloheimo05@gmail.com>
2026-09-23 23:32:53 +0300

    add model training

--- week4/stock_market_prediction.py
+++ week4/stock_market_prediction.py
@@ -3,19 +3,19 @@ import numpy as np
 from numpy.lib.stride_tricks import sliding_window_view
 import torch.nn as nn
 import torch.nn.functional as F
+import torch
 
 class LinearModel(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self):
         super().__init__()
 
-        self.linear1 = nn.Linear(100, 20)
+        self.linear1 = nn.Linear(100, 10)
         self.output = nn.Linear(10, 1)
 
     def forward(self, x):
         y = self.linear1(x)
         y = F.relu(y)
         y = self.output(y)
-        y = F.relu(y)
         return y
 
 def create_dataset(prices):
@@ -33,8 +33,9 @@ def main():
 
     X_all_tr = []
     Y_all_tr = []
-    
-    for i, ticker in enumerate(tickers):
+
+    # Form training data
+    for ticker in tickers:
         # Load data
         data = yf.download(ticker, '2020-01-01', '2025-12-31',
                            group_by=ticker, auto_adjust=False,
@@ -46,7 +47,29 @@ def main():
 
         X_all_tr.append(X)
         Y_all_tr.append(Y)
-        
+
+    X = torch.from_numpy(X_all_tr[0].copy()).float()
+    Y = torch.from_numpy(Y_all_tr[0].copy()).float()
+
+    model = LinearModel()
+
+    criterion =  nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+    for epoch in range(1000):
+        optimizer.zero_grad()
+
+        Y_pred = model(X)
+
+        loss = criterion(Y_pred, Y)
+
+        loss.backward()
+
+        optimizer.step()
+
+        if epoch % 100 == 0:
+            print(f'epoch: {epoch}, loss: {loss.item()}')
+
 
 
 if __name__ == "__main__":
